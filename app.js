4d8459cb8894d6f0da8723782d994d22ec38e536
yluo12 <yantingluo98@gmail.com>
2023-04-10 11:44:20 -0700

    finished the exercise

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-// TODO
+const GroceryList = () => (
+  <>
+    <h2>Grocery List</h2>
+    <GroceryListItem lists={['Bread', 'Peanut Butter', 'Jam']} />
+  </>
+);
+
+const EachItem = (props) => {
+  const [isBold, setIsBold] = React.useState(false);
+  const style = {
+    fontWeight: isBold ? 'bold' : 'normal'
+  };
+  return (<li style={style}
+    onMouseEnter={() => setIsBold(!isBold)}
+    onMouseLeave={() => setIsBold(!isBold)}>
+      {props.item}
+      </li>);
+};
+
+const GroceryListItem = (props) => {
+  const onListItemClick = (event) => {
+    console.log('I got clicked');
+  };
+
+  return (
+    <ul>
+      {props.lists.map((item) => (
+        <EachItem item={item}/>
+      ))}
+    </ul>
+  );
+};
+
+
+ReactDOM.render(<GroceryList />, document.getElementById('app'));
